d093f5fd8d75b39404769393e05923f61507901d
JosMuse <no-reply>
2021-01-21 22:39:09 -0700

    so stoked on this outcome

--- src/components/home.js
+++ src/components/home.js
@@ -8,20 +8,46 @@ class Main extends Component {
     return (
       <div>
         <div className="main-content">
-          <Container>
+          <Container id="container">
             <Row>
               <Col className="movedown" xs={12} md={1}></Col>
               <Col className="movedown" xs={12} md={6}>
-                <div className="z">
-                  <div id="images">
-                    <span className="hide">
-                      THIS IS IMAGE FILLER TO MAKE IT WORK I THINK I CAN DO THIS
-                    </span>
+                <div className="mainNav">
+                
+                  <div id="design" className="crunch">
+                    <span className="numbers">01</span>
+                    <span className="font-styling">DESIGN</span>
                   </div>
+                  <div id="photo" className="crunch">
+                    <span className="numbers">02</span>
+                    <span className="font-styling">PHOTO</span>
+                  </div>
+                  <div id="video" className="crunch">
+                    <span className="numbers">03</span>
+                    <span className="font-styling">VIDEO</span>
+                  </div>
+                  <div id="music" className="crunch">
+                    <span className="numbers">04</span>
+                    <span className="font-styling">MUSIC</span>
+                  </div>
+                  <div id="image2"></div>
                 </div>
-                <div className="crunch" id="design">
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Main;
+
+{
+  /* <div id="image">hello</div>
+                <div className="crunch">
                   <span className="numbers">01</span>
-                  <span className="font-styling">DESIGN</span>
+                  <span className="font-styling" id="design">DESIGN</span>
                 </div>
                 <div className="crunch">
                   <span className="numbers">02</span>
@@ -41,14 +67,5 @@ class Main extends Component {
                   <span className="font-styling" id="music">
                     MUSIC
                   </span>
-                </div>
-              </Col>
-            </Row>
-          </Container>
-        </div>
-      </div>
-    );
-  }
+                </div> */
 }
-
-export default Main;
